dc795fa446482428ae56f02e8574a0846d25ece6
Liza <fildepiorsta@gmail.com>
2024-04-21 18:59:47 +0300

    modal ect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import StatusLabel, { Status } from './components/status-label';
 import { headers } from 'next/headers';
+import AddCompanyButton from './components/add-company-button';
 
 export default function Home() {
  
   return (
     <main>
       <h1 className="text-xl">Home Page</h1>
+      <AddCompanyButton/>
       <StatusLabel status={Status.Active}>Active</StatusLabel>
       <StatusLabel status={Status.NotActive}>NotActive</StatusLabel>
       <StatusLabel status={Status.Pending}>Pending</StatusLabel>
